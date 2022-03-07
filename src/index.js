427263b89af129282ae382c1c087a978dbf3787a
Alos22 <aniriver689@gmail.com>
2022-03-07 17:34:09 -0500

    intialized project, navbar in progress

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./public/styles.css"
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//IMPORT BrowserRouter and rename it to Router
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Main from "./pages/Main"
+//Wrap the App Component with the Router component to enable the router features
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Nav />
+      <Routes >
+        <Route path="/" element={<App />} >
+          <Route index element={<Main />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
