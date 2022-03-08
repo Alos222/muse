ee03ea05bad0f8c200e0a83b65ff49c8d103f84c
Alos22 <aniriver689@gmail.com>
2022-03-07 21:05:46 -0500

    main page progress

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,23 +6,20 @@ function Main(props) {
     return (
         <Container fluid className={"no-gutters mx-0 px-0"}>
             <Row>
-                <img src={Hero} className="hero_img" alt="hero image" /><div className="hero_title">John Singer Sergant</div>
+                <img src={Hero} className="hero_img" alt="hero image" />
             </Row>
             <Row>
                 <Col sm={6}>
-                    <Row  className="justify-content-md-center">
-                    <Col sm={6}>
-                    <Stack gap={3}>
-                    <div className="About">
-                    <h2>About Muse</h2>
-                    <p>Est ex exercitation Lorem dolore eiusmod tempor. Ullamco Lorem sint est laborum. Irure sunt in culpa voluptate pariatur nostrud duis sit. Qui dolore minim reprehenderit laborum nulla sit cillum et elit cupidatat nulla non. Eiusmod minim id proident aliqua cupidatat excepteur exercitation proident anim. Ut eu veniam fugiat aliquip exercitation fugiat adipisicing pariatur nostrud dolor. Pariatur et dolor magna laborum eu cupidatat mollit deserunt dolor cupidatat ex.</p>
-                    </div>
-                    </Stack>
-                 
-                    </Col>
-
+                    <Row className="justify-content-md-center">
+                        <Col sm={6}>
+                            <Stack gap={3}>
+                                <div className="About">
+                                    <h2>About Muse</h2>
+                                    <p>Est ex exercitation Lorem dolore eiusmod tempor. Ullamco Lorem sint est laborum. Irure sunt in culpa voluptate pariatur nostrud duis sit. Qui dolore minim reprehenderit laborum nulla sit cillum et elit cupidatat nulla non. Eiusmod minim id proident aliqua cupidatat excepteur exercitation proident anim. Ut eu veniam fugiat aliquip exercitation fugiat adipisicing pariatur nostrud dolor. Pariatur et dolor magna laborum eu cupidatat mollit deserunt dolor cupidatat ex.</p>
+                                </div>
+                            </Stack>
+                        </Col>
                     </Row>
-               
                 </Col>
                 <Col sm={6}>
                     <img src={Met} className="met_img" alt="The Met" />
