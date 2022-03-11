9ab0a355919e2998c44ace011c75513f6ac00b15
Alos22 <aniriver689@gmail.com>
2022-03-10 19:24:47 -0500

    index and app switched for user auth, user auth still in progress

--- src/App.js
+++ src/App.js
@@ -1,16 +1,88 @@
-import React from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Import route and our components
-import { Outlet } from "react-router";
+//Components
+import Nav from "./components/Nav";
+import Main from "./pages/Main"
+import Dashboard from "./pages/Dashboard"
+import Context from './context/context';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+
+import { useEffect, useState, React } from 'react';
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+
+
 
 function App() {
-  // We will use the Route component to specify each route
+
+  const [error, setError] = useState();
+  const [userCredentials, setUserCredentials] = useState({
+
+    token: null,
+    user: null,
+
+  });
+
+  useEffect(() => {
+
+    const checkLoggedIn = async () => {
+
+      let token = localStorage.getItem('auth-token');
+
+      if (token === null) {
+        localStorage.setItem('auth-token', '');
+        token = '';
+      }
+
+      const tokenResponse = await axios.post
+        (
+          'http://localhost:6000/api/auth/tokenIsValid',
+          null,
+          {
+            headers: {
+              'x-auth-token': token
+            }
+          }
+        ).catch((error) => {
+          console.log(error.toJSON());
+        });
+
+      if (tokenResponse) {
+        const res = await axios.get
+          (
+            'http://localhost:6000/api/auth/tokenIsValid',
+            {
+              headers: {
+                'x-auth-token': token
+              }
+            }).catch((error) => {
+              setError(error);
+            });
+
+        setUserCredentials({
+          token,
+          user: res,
+        });
+      }
+    }
+
+    checkLoggedIn();
+
+  }, []);
+
   return (
-    <div>
-      <Outlet />
-    </div>
+      <Context.Provider value={{ userCredentials, setUserCredentials }}>
+        <Nav />
+        <Routes >
+          <Route path="/" element={<App />} />
+          <Route index element={<Main />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Context.Provider>
   );
 }
 
