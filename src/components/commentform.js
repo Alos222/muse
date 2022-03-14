48b4e2fbcde1f90afbd21871cda6af5250d7c706
Alos22 <aniriver689@gmail.com>
2022-03-14 00:28:26 -0400

    comment list now displaying on artview page

--- src/components/commentform.js
+++ src/components/commentform.js
@@ -1,32 +1,58 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 
 const AddComment = (props) => {
     console.log(props)
 
-    const [userCredentials, setUserCredentials] = useState()
+    const params = useParams()
+    const artKey = params.objectID
+
+    const url = `http://localhost:3001/api/comment/${artKey}`
+
 
     const [username, setUsername] = useState('');
-    const [artId, setArtId] = useState('');
+    const [artId, setArtId] = useState();
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [userId, setUserId] = useState('');
+    const [commentList, setCommentList] = useState('')
+    const [mappedComments, setMappedComments] = useState('')
+
+    
+
 
     useEffect(() => {
         const getUser = async () => {
             const id = props.user._id
             const username = props.user.username
             const artworkID = props.art.objectID
-            console.log(artworkID)
-
             setArtId(artworkID);
             setUserId(id);
             setUsername(username);
         };
         getUser();
+        const getComments = async () => {
+            const response = await fetch(url)
+            const data = await response.json()
+            setCommentList(data)
+        }
+        getComments().then( function(){
+            const mapComments = async () => {
+                const data = commentList.map((comment) => (
+                    <div>
+                        <h5>{comment.title}</h5>
+                        <p>{comment.body}</p>
+                    </div>
+                ))
+                setMappedComments(data);
+            }
+            mapComments();
+         
+        }
+            );
     }, [props]);
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -50,6 +76,7 @@ const AddComment = (props) => {
 
     return (
         <div>
+            {mappedComments}
             <h1></h1>
             <h4>Comment</h4>
             <form onSubmit={(e) => (handleSubmit(e))}>
